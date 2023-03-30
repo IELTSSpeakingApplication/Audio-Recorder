0a88ed8485a61ca3d442a6d8d6f03f395f00c939
Hafid Ikhsan Arifin <harifin315@gmail.com>
2023-03-30 15:44:53 +0700

    Add get_available_devices function audiorecorder.py

--- audiorecorder.py
+++ audiorecorder.py
@@ -4,8 +4,12 @@ import sounddevice as sd
 import soundfile as sf
 from queue import Queue
 
+def get_available_devices():
+    print(f"\nAvailable devices:")
+    print(sd.query_devices())
+
 def main():
-    print("Audio Recorder")
+    get_available_devices()
 
 if __name__ == "__main__":
     main()
