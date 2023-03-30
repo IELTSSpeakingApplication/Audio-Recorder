4645384a8bddf0a640fd994b90a568a7e6cf1346
Hafid Ikhsan Arifin <harifin315@gmail.com>
2023-03-30 16:13:30 +0700

    Add get_input_device function and use try catch in main audiorecorder.py

--- audiorecorder.py
+++ audiorecorder.py
@@ -8,8 +8,24 @@ def get_available_devices():
     print(f"\nAvailable devices:")
     print(sd.query_devices())
 
+def get_input_device():
+    print(f"\n* Choose input device (1/2 in, 0 out)")
+    print(f"* Default microphone (>)")
+
+    try:
+        device = int(input(f"\nDevice number: "))
+    except ValueError:
+        sys.exit("Input device must be integer (1/2/3/..)")
+    
+    return device
+
 def main():
-    get_available_devices()
+    try:
+        get_available_devices()
+        device_number = get_input_device()
+        print(device_number)
+    except Exception as e:
+        sys.exit(f"\nOpss, Something went wrong:", str(e))
 
 if __name__ == "__main__":
     main()
