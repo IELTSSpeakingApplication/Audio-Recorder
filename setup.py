279c31ea31a27ed23d9a3cb14adf643c5b88fe82
Hafid Ikhsan Arifin <harifin315@gmail.com>
2023-03-31 11:18:12 +0700

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+with open("README", 'r') as f:
+    long_description = f.read()
+
+setup(
+   name='Audio Recorder',
+   version='1.0.0',
+   description='Audio Recorder Application',
+   license="MIT",
+   long_description=long_description,
+   author='Hafid Ikhsan Arifin',
+   author_email='hafidikhsanarifin@gmail.com',
+   url="https://github.com/IELTSSpeakingApplication/Audio-Recorder",
+)
